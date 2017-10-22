dcc65a9f64473d335f47492825db2f4f260f698a
Kotov <cat-kotov@yandex.ru>
2017-10-23 01:00:23 +0300

    Create test environment

--- modules/cat-eye-common/cat-eye-common-util/src/main/java/org/cat/eye/common/util/file/JarFileUtil.java
+++ modules/cat-eye-common/cat-eye-common-util/src/main/java/org/cat/eye/common/util/file/JarFileUtil.java
@@ -2,6 +2,7 @@ package org.cat.eye.common.util.file;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
 import java.util.jar.JarEntry;
@@ -9,7 +10,11 @@ import java.util.jar.JarFile;
 
 public class JarFileUtil {
 
+    private final static String FILE_SEPARATOR = "/";
+
     public static List<String> getClassesNames(String jarFilePath) {
+
+        List<String> result = new ArrayList<>();
 
         File file = new File(jarFilePath);
 
@@ -20,12 +25,11 @@ public class JarFileUtil {
                 if (jarEntities != null) {
                     while (jarEntities.hasMoreElements()) {
                         JarEntry jarEntry = jarEntities.nextElement();
-
-
+                        if (jarEntry.getName().endsWith(".class")) {
+                            result.add(parseJarEntryName(jarEntry.getName()));
+                        }
                     }
-
                 }
-
             } catch (IOException e) {
 
             }
@@ -33,6 +37,10 @@ public class JarFileUtil {
 
         }
 
-        return null;
+        return result;
+    }
+
+    private static String parseJarEntryName(String jarEntryName) {
+        return jarEntryName.replace(FILE_SEPARATOR, ".").substring(1, jarEntryName.lastIndexOf('.'));
     }
 }
