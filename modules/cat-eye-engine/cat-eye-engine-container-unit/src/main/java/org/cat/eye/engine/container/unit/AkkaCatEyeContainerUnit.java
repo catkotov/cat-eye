72ec3baf6d57e82d183345408ac1a67a8323946f
cat-kotov <cat-kotov@yandex.ru>
2019-01-15 19:35:11 +0300

    Akka container implementation created

--- modules/cat-eye-engine/cat-eye-engine-container-unit/src/main/java/org/cat/eye/engine/container/unit/AkkaCatEyeContainerUnit.java
+++ modules/cat-eye-engine/cat-eye-engine-container-unit/src/main/java/org/cat/eye/engine/container/unit/AkkaCatEyeContainerUnit.java
@@ -1,11 +1,13 @@
 package org.cat.eye.engine.container.unit;
 
+import akka.actor.ActorSystem;
+
 /**
  * Created by Kotov on 28.10.2018.
  */
 public class AkkaCatEyeContainerUnit {
 
-
+    private ActorSystem actorSystem = ActorSystem.create("cat-eye-container-unit-actor-system");
 
     public void initialize() {
 
