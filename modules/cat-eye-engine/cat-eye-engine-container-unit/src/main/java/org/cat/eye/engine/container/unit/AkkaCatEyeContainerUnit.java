5fd874e88ffa2bfe1d76f00015af9e8366df7934
Kotov <cat-kotov@yandex.ru>
2019-01-15 22:41:41 +0300

    New Akka based container for Unit testing.

--- modules/cat-eye-engine/cat-eye-engine-container-unit/src/main/java/org/cat/eye/engine/container/unit/AkkaCatEyeContainerUnit.java
+++ modules/cat-eye-engine/cat-eye-engine-container-unit/src/main/java/org/cat/eye/engine/container/unit/AkkaCatEyeContainerUnit.java
@@ -1,13 +1,31 @@
 package org.cat.eye.engine.container.unit;
 
+import org.cat.eye.engine.common.deployment.BundleDeployer;
+import org.cat.eye.engine.common.deployment.management.BundleManager;
+import org.cat.eye.engine.common.deployment.management.BundleManagerImpl;
+import org.cat.eye.engine.container.unit.deployment.UnitBundleDeployerImpl;
+
 /**
  * Created by Kotov on 28.10.2018.
  */
 public class AkkaCatEyeContainerUnit {
 
+    private BundleDeployer bundleDeployer;
 
+    private String pathToClasses;
+
+    private String bundleDomain;
+
+    public AkkaCatEyeContainerUnit(String pathToClasses, String bundleDomain) {
+        this.pathToClasses = pathToClasses;
+        this.bundleDomain = bundleDomain;
+        this.bundleDeployer = new UnitBundleDeployerImpl();
+        this.bundleDeployer.setBundleManager(new BundleManagerImpl());
+    }
 
     public void initialize() {
+        // deploy bundle
+        bundleDeployer.deploy(pathToClasses, bundleDomain);
 
     }
 }
